7fcfbceef3a3274d97dccb8be3542cf4513feb5e
Hsuan Wang <jessica.w@inwinstack.com>
2022-11-08 13:54:58 -0800

    new branch

---
+++ TetraStar/src/TObject/Test.java
@@ -0,0 +1,10 @@
+package TObject;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello please try to do your homework by yourself ：）");
+	}
+
+}
