435715b32221ad8617f9f62181771836d0333792
Alex Wu <alexwu727@gmail.com>
2022-11-14 13:22:13 -0800

    main test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,50 @@
 package main.java;
 
+import main.java.Base.HeroBase;
+import main.java.Base.MapBase;
+import main.java.Inhabitant.Inhabitant;
 import main.java.Inhabitant.TetHero;
 import main.java.Inhabitant.TetRover;
+import main.java.Inhabitant.TetVader;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        // TFace tFace = new TFace(10, 10);
-        // TetRover t1 = new TetRover(3, 4, 1);
-        // TetRover t2 = new TetHero(5, 8, 2);
-        // TetHero t3 = new TetHero(1, 1, 3);
-        // // TetRover t3 = new TetHero(1, 1);
-        // t1.setTFace(tFace);
-        // t2.setTFace(tFace);
-        // t3.setTFace(tFace);
 
+        TFace tFace = new TFace(10, 10);
+        List<TetRover> InhibitantList = new ArrayList<>();
+        MapBase b1 = new MapBase(8, 8);
+        TetRover t1 = new TetRover(3, 4, 1, tFace);
+        TetHero t2 = new TetHero(5, 8, 2, tFace);
+        TetHero t3 = new TetHero(1, 1, 3, tFace);
+        TetVader t4 = new TetVader(6, 7, 4, tFace);
+        t1.setDisplayID(1);
+        t2.setDisplayID(2);
+        t3.setDisplayID(3);
+        t4.setDisplayID(4);
+
+        InhibitantList.add(t1);
+        InhibitantList.add(t2);
+        InhibitantList.add(t3);
+        InhibitantList.add(t4);
+        while (true) {
+            for (TetRover tetRover : InhibitantList) {
+                tetRover.action();
+            }
+            bar();
+            tFace.printSurface();
+            wait(1000);
+        }
+        // TetRover t3 = new TetHero(1, 1);
         // // System.out.println(t1.getRow() + " " + t1.getCol());
         // // let 1 be hero, 2 be vader, 3 be rover, 4 be
         // tFace.addObject(t1);
@@ -33,35 +56,37 @@ public class Main {
         // tFace.addBase(2, 6, 4);
         // tFace.addBase(2, 8, 4);
         // tFace.printSurface();
-        JFrame frame = new JFrame("Test title");
+        // JFrame frame = new JFrame("Test title");
 
-        frame.setSize(600, 600);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // frame.setSize(600, 600);
+        // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // // JPanel panel = new JPanel();
+        // // panel.setBackground(Color.BLUE);
+        // frame.setLayout(new GridLayout(10, 10));
+        // ImageIcon imageIcon = new ImageIcon("res/hero.png"); // load the image to a
+        // imageIcon
+        // Image image = imageIcon.getImage(); // transform it
+        // Image newimg = image.getScaledInstance(20, 20, java.awt.Image.SCALE_SMOOTH);
+        // // scale it the smooth way
+        // imageIcon = new ImageIcon(newimg);
+        // JLabel picLabel = new JLabel(imageIcon);
+        // for (int i = 0; i < 10; i++) {
+        // for (int j = 0; j < 10; j++) {
         // JPanel panel = new JPanel();
-        // panel.setBackground(Color.BLUE);
-        frame.setLayout(new GridLayout(10, 10));
-        ImageIcon imageIcon = new ImageIcon("res/hero.png"); // load the image to a imageIcon
-        Image image = imageIcon.getImage(); // transform it
-        Image newimg = image.getScaledInstance(20, 20, java.awt.Image.SCALE_SMOOTH); // scale it the smooth way
-        imageIcon = new ImageIcon(newimg);
-        JLabel picLabel = new JLabel(imageIcon);
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                JPanel panel = new JPanel();
-                panel.setLayout(new FlowLayout());
-                panel.add(picLabel);
-                // JButton btn = new JButton("1");
+        // panel.setLayout(new FlowLayout());
+        // panel.add(picLabel);
+        // // JButton btn = new JButton("1");
 
-                // btn.setBackground(Color.RED);
-                // btn.setForeground(Color.GREEN);
-                // btn.setContentAreaFilled(false);
-                // btn.setOpaque(true);
-                frame.add(panel);
-            }
-        }
-
+        // // btn.setBackground(Color.RED);
+        // // btn.setForeground(Color.GREEN);
+        // // btn.setContentAreaFilled(false);
+        // // btn.setOpaque(true);
         // frame.add(panel);
-        frame.setVisible(true);
+        // }
+        // }
+
+        // // frame.add(panel);
+        // frame.setVisible(true);
     }
 
     // for (Map.Entry<int[], Integer> entry : tFace.baseMap.entrySet()) {
