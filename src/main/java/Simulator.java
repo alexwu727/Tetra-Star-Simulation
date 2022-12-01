8cf34c56dd297c1ca9b20d0f014dd38ccef6d8e7
Alex Wu <alexwu727@gmail.com>
2022-12-01 15:07:46 -0800

    add different scenarios

--- src/main/java/Simulator.java
+++ src/main/java/Simulator.java
@@ -6,20 +6,29 @@ import java.util.HashMap;
 
 public class Simulator {
     static Scenario scenario;
+    static int currentScenarioIndex;
+    static int helper = 10;
     private static HashMap<Integer, Scenario> scenarioHashMap = new HashMap<Integer, Scenario>();
 
     public static void start(int scenarioIndex) {
         TFace.clear();
         scenario = scenarioHashMap.get(scenarioIndex);
+        currentScenarioIndex = scenarioIndex;
         TFace tFace = TFace.instance();
         tFace.setSurfaceSize(scenario.sufaceRowSize, scenario.sufaceColSize);
         scenario.createInstances();
     }
 
     public static void nextFrame() {
+        helper -= 1;
         for (TetRover tetRover : scenario.inhibitantList) {
             tetRover.action();
         }
+        if (helper == 0 && currentScenarioIndex == 3) {
+            scenario.inhibitantList.get(0).setWalkDirections(1);
+            scenario.inhibitantList.get(1).setWalkDirections(3);
+        }
+
     }
 
     public static void wait(int ms) {
@@ -40,10 +49,10 @@ public class Simulator {
         scenario.sufaceRowSize = 10;
         scenario.sufaceColSize = 10;
         scenario.addTetHeroArgs(0, 0, 1, 0);
-        scenario.addTetHeroArgs(3, 3, 2, 0);
-        scenario.addTetVaderArgs(3, 1, 3, 0);
-        scenario.addStarMapArgs(1, 1, 0, "hello world");
-        scenario.addStarAltasArgs(1, 2, 0, new int[] { 1, 2, 3, 4 }, new String[] { "hel", "lo", "wor", "ld" });
+        scenario.addTetHeroArgs(5, 9, 2, 0);
+        scenario.addTetVaderArgs(4, 4, 3, 0);
+        scenario.addStarMapArgs(7, 2, 0, "hello world");
+        scenario.addStarAltasArgs(2, 7, 0, new int[] { 1, 2, 3, 4 }, new String[] { "hel", "lo", "wor", "ld" });
         scenarioHashMap.put(0, scenario);
 
         // scenario 1 - hero enters the mapbase with a map
@@ -64,21 +73,13 @@ public class Simulator {
         scenario.addStarMapArgs(3, 4, 0, "hello world");
         scenarioHashMap.put(2, scenario);
 
-        // scenario 3 - hero enters the mapbase with a encrypted map (by him)
+        // scenario 3 - hero enters the mapbase with a encrypted map (by others)
         scenario = new Scenario();
         scenario.sufaceRowSize = 10;
         scenario.sufaceColSize = 10;
         scenario.addTetHeroArgs(3, 0, 1, 4);
-        scenario.addTetVaderArgs(6, 4, 2, 1);
-        scenario.addStarMapArgs(3, 4, 0, "hello world");
-        scenarioHashMap.put(3, scenario);
-
-        // scenario 4 - hero enters the mapbase with a encrypted map (by others)
-        scenario = new Scenario();
-        scenario.sufaceRowSize = 10;
-        scenario.sufaceColSize = 10;
-        scenario.addTetHeroArgs(3, 0, 1, 4);
-        scenario.addTetVaderArgs(6, 4, 2, 1);
+        scenario.addTetHeroArgs(3, 9, 2, 4);
+        scenario.addTetVaderArgs(5, 4, 3, 1);
         scenario.addStarMapArgs(3, 4, 0, "hello world");
         scenarioHashMap.put(3, scenario);
     }
